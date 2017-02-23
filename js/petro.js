a302abcb0634667fdde3df164de6eeae3c930bde
Caroline Nygaard Sjøtlov <CarolineSjotlov@desktop-0larddt.kea.local>
2017-02-23 12:38:04 +0100

    add javascript template produktliste

--- js/petro.js
+++ js/petro.js
@@ -1,8 +1,15 @@
-$.getJSON("http://petlatkea.dk/2017/dui/api/productlist", jegHarFåetData)
+window.addEventListener("load", sidenVises);
 
-function jegHarFåetData(retter) {
-    console.log(retter)
+function sidenVises() {
+    console.log("siden vises");
+    visProdukt();
+}
 
+function visProdukt() {
+    //klon produkt_template
+    var klon = document.querySelector("#produkt_template").content.cloneNode(true);
+    //indsæt data i klon
 
-
+    //append klon til .produktliste
+    document.querySelector(".produktliste").appendChild(klon);
 }
